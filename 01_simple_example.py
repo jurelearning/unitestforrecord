cd92c4dd637cf9573a2f035b87501d7c903bb827
doris <dorischuwork@gamil.com>
2022-06-12 20:37:59 +0800

    [Add] add files

---
+++ 01_simple_example.py
@@ -0,0 +1,18 @@
+
+def new_sum(iterable):
+    result = 0
+    for val in iterable:
+        result += val
+    return result
+
+def test_new_sum_list():
+    assert new_sum([1, 2, 3]) == 6
+    
+def test_new_sum_tuple():
+    assert new_sum((-1, 2, 3)) == 6
+
+if __name__ == "__main__":
+    test_new_sum_list()
+    test_new_sum_tuple()
+
+    # Unit tests are not perfect and it is near impossible to achieve 100% code coverage
