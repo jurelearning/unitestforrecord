a9eb3ddff665978ebaacaa8f76edf061fb0142ee
doris <dorischuwork@gamil.com>
2022-06-12 21:41:10 +0800

    [Add] add cover

---
+++ 06_Coverage/test_code.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from unittest.mock import patch
+from codebase import pipeline
+
+
+
+# run cmd: pytest --cov=codebase
+# If there were any lines that we note covered, then that value would be shown under Miss
